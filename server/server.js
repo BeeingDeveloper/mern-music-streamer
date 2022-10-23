7b0737b8cd0a253dd9a77fe2320e20dab7d39d75
BeeingDeveloper <com.official.akhter007@gmail.com>
2022-10-23 11:17:36 +0530

    Loging with google UI setup

---
+++ server/server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+
+
+// ---------------------------------------------------------------
+//SETUP MIDDLEWARE:
+app.use(cors());
+app.use(express.json());
+// ---------------------------------------------------------------
+
+
+
+
+// ---------------------------------------------------------------
+//MONGOOSE CONNECT:
+// const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri, 
+//     {useNewUrlParser: true, useUnifiedTopology:true,}
+// );
+// const connection = mongoose.connection;
+// connection.once('open', ()=>{
+//     console.log("Mongodb connection established successfully...");
+// })
+// ---------------------------------------------------------------
+
+
+
+
+// ACCESS FILES--------------------------------------------------
+
+// const exerciseRouter = require('./routes/exercises');
+// const userRouter = require('./routes/users');
+
+// app.use('/exercises', exerciseRouter);
+// app.use('/users', userRouter);
+// ---------------------------------------------------------------
+
+
+
+
+
+//START SERVER:
+app.get('/', (req, res)=>{
+    return res.json("Hi there...");
+})
+
+app.listen(port, ()=>{
+    console.log(`Server running at port: ${port}`);
+})
+// ---------------------------------------------------------------
